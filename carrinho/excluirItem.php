729a834fdffa80cb52c54e9dd0bfdf236231803b
Joás Martins <129910407+JoasMartins@users.noreply.github.com>
2025-11-16 20:59:31 -0300

    Delete carrinho directory

--- carrinho/excluirItem.php
+++
@@ -1,8 +0,0 @@
-<?php include("../config-database.php"); ?>
-
-<?php 
-
-$id = $_POST['id'];
-
-$sql = "DELETE FROM `carrinho` WHERE id = '$id'";
-$resultado = $MySQLi->query($sql) OR trigger_error($MySQLi->error, E_USER_ERROR);
--- carrinho/limparCarrinho.php
+++
@@ -1,6 +0,0 @@
-<?php include("../config-database.php"); ?>
-
-<?php 
-
-$sql = "DELETE FROM `carrinho`";
-$resultado = $MySQLi->query($sql) OR trigger_error($MySQLi->error, E_USER_ERROR);
